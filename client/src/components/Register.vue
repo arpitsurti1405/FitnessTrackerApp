f9f74411520cdf1f0614842dcdcaebbddc450771
Arpit <arpitsurti@rediffmail.com>
2019-03-17 12:16:01 -0400

    Done Database connection, registration api.

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,29 +1,54 @@
 <template>
-  <div>
-    <h1>Registration</h1>
-    Email: <input type="email" name="email" placeholder="Email" v-model="email" /><br/><br/>
-    Password: <input type="password" name="password" placeholder="Password" v-model="password" /><br/><br/>
-    <button @click="register">Register</button>
-  </div>
-</template>
+  <v-layout column>
+    <v-flex xs6 offset-xs3>
+      <div class="white elevation-2">
+        <v-toolbar flat dense class="cyan" dark>
+          <v-toolbar-title>Register</v-toolbar-title>
+        </v-toolbar>
+
+        <div class="pl-4 pr-4 pt-2 pb-2">
+          <form name="registration" autocomplete="off">
+            <v-text-field label="Email" v-model="email"></v-text-field>
+            <br>
+
+            <v-text-field label="Password" type="password" v-model="password" autocomplete="new-password"></v-text-field>
+          </form>
+          <br>
+
+          <div class="error" v-html="error" /><br>
+          <v-btn dark class="cyan" @click="register">Register</v-btn>
+        </div>
+      </div>
+    </v-flex>
+  </v-layout>
+ </template>
 <script>
 import AuthenticationService from '@/services/AuthenticationService'
 export default{
-  data(){
-    return{
-      email:  '',
-      password: ''
+  data () {
+    return {
+      email: '',
+      password: '',
+      error: null
     }
   },
-  methods:{
-    async register (){
-     await AuthenticationService.register({
-       email:this.email,
-       password:this.password
-     })
+  methods: {
+    async register () {
+      try {
+        await AuthenticationService.register({
+          email: this.email,
+          password: this.password
+        })
+      } catch (error) {
+        this.error = error.response.data.error
+      }
     }
   }
 }
 </script>
-<style>
+<style scoped>
+.error{
+  color:red;
+}
+
 </style>
